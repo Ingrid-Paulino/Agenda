dad72873d87c31c471f145c6cce680bafced1537
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-06-13 16:38:37 -0300

    :sparkles: feat: adicionado conteudo na pagina de CustomerProfile

--- app/frontend/src/pages/CustomerProfile.tsx
+++ app/frontend/src/pages/CustomerProfile.tsx
@@ -1,3 +1,44 @@
+import React from "react";
+
+import { RootState } from "../Stock_redux_toolkit/store";
+import { useSelector } from "react-redux";
+
+import { useNavigate } from "react-router-dom";
+
+
+import md5 from "crypto-js/md5";
+import Header from "../components/Header";
+
 export default function CustomerProfile() {
-  return <h1>Oiiiii</h1>;
+  const navigate = useNavigate();
+
+  const stock = useSelector((state: RootState) => state.stock);
+
+  const gravatar = md5(stock.myReducer.login.email).toString();
+
+  function voltarPage() {
+    navigate("/customerDay");
+  }
+
+  return (
+    <>
+      <Header
+        titlePage="PROFILE"
+        existButtonInHeader={true}
+        textButtonRegister="VOLTAR"
+        funcLoginAndRegister={() => voltarPage()}
+      />
+
+      <div>
+        <img
+          // data-testid="header-profile-picture"
+          src={`https://www.gravatar.com/avatar/${gravatar}`}
+          alt="Imagem gravatar"
+        />
+      </div>
+
+      <p>Puxarei o nome do banco de dados</p>
+      <p>Puxarei o endereço do banco de dados</p>
+    </>
+  );
 }
