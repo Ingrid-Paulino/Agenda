bfcbec4b5a7898778c63af1d2b5c54021f1f3f97
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-06-13 15:00:27 -0300

    :sparkles: feat: adicionado inputs e cabeçalho na pagina de customerHour

--- app/frontend/src/pages/CustomerHour.tsx
+++ app/frontend/src/pages/CustomerHour.tsx
@@ -1,9 +1,43 @@
 import React from "react";
+import Header from "../components/Header";
+
+import { useSelector } from "react-redux";
+import { RootState } from "../Stock_redux_toolkit/store";
+import Input from "../components/Input";
+import Select from "../components/Select";
 
 export default function CustomerHour() {
+  const stock = useSelector((state: RootState) => state.stock);
+
   return (
     <>
-      <h1>OIIII</h1>
+      <Header
+        titlePage="HORARIO"
+        existButtonInHeader={true}
+        textButtonRegister={stock.myReducer.login.email}
+      />
+      <main>
+        <h1>CONFIRME SEU HORARIO</h1>
+        <form action="">
+          <Input
+            labelInput="NOME"
+            func={(event) => handleChange(event, setPassword)}
+          />
+          <Input
+            labelInput="DATA"
+            func={(event) => handleChange(event, setPassword)}
+          />
+          <Select
+            labelInput="NOME"
+            func={(event) => handleChange(event, setPassword)}
+          />
+          <Select />
+          <Input
+            labelInput="PREÇO"
+            func={(event) => handleChange(event, setPassword)}
+          />
+        </form>
+      </main>
     </>
   );
 }
