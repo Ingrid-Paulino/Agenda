9e7d645bd3705bfc0530297d307047d2efb85ef3
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-06-24 17:59:55 -0300

    :sparkles: feat:  adicionado um novo tipo de retorno quando não existir usuarios cadastrados na tabela de clients

---
+++ app/backend/src/app/services/registrationServices.ts
@@ -0,0 +1,34 @@
+import { IClient, Client } from '../interface';
+import { ClientDAO } from '../../db/db_sequelize';
+import RegisterModel from '../models/model';
+import entryMsgStatusError from '../helpers/entryMsgStatusError';
+import { MSG, StatusCodes } from '../enum/enumStatusAndMessage';
+import descriptografia from '../utils/descriptografia';
+import bcrypt from 'bcryptjs';
+
+
+const getAll = async (): Promise<Client[]> => {
+  const result = await RegisterModel.getAll(ClientDAO);
+  if (!result) throw entryMsgStatusError(StatusCodes.OK, '[]');
+  return result;
+};
+
+const create = async (data: IClient): Promise<Client> => {
+  const clientsAll = await getAll();
+
+  const hash = bcrypt.hashSync(data.password, 10);
+  const check = descriptografia(data.password, hash);
+  if (!check) throw entryMsgStatusError(StatusCodes.UNAUTHORIZED, MSG.INCORRECT_PASSWORD);
+
+  const findClient = clientsAll.find((client: IClient) => client.email === data.email);
+  if (findClient) throw entryMsgStatusError(StatusCodes.CONFLICT, MSG.EXISTING_USER);
+
+  const result = await RegisterModel.create(data, ClientDAO, hash);
+  return result;
+};
+
+export default {
+  create,
+  getAll
+};
+
